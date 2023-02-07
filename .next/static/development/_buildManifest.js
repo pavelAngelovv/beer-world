4f94eb8f18871b0ad6c9f05e2efa620417cd18aa
pavelcodixis <pangelov@codixis.com>
2023-02-07 12:43:10 +0200

    Removed .next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/beers":["static\u002Fchunks\u002Fpages\u002Fbeers.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fbeers"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

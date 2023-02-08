bca4dbdcb68ea560c70e56802157f429231b1e4b
pavelcodixis <pangelov@codixis.com>
2023-02-08 14:23:08 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,11 @@
-import { styled } from "@mui/material/styles";
 import React from "react";
 
+import { Box } from "@mui/material";
+
 export const Footer = () => {
-  const Root = styled("div")(({ theme }) => ({
-    padding: theme.spacing(1),
-
-    [theme.breakpoints.up("md")]: {
-      marginLeft: "6.7cm",
-    },
-  }));
-
   return (
-    <Root>
+    <Box sx={{ marginLeft: { md: "6.7cm", sm: "0" } }}>
       <footer>@Copyright 2023 All rights reserved</footer>
-    </Root>
+    </Box>
   );
 };
